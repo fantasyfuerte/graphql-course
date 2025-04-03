55d69465372f9fcd5badb2a90d334d966f6d0b91
fuerte <leandrofuerte75@gmail.com>
2025-04-03 20:32:20 +0100

    Refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   return (
     <main>
-      <h1 className="text-red-400">
-        Fetching data from Apollo Graphql server
-      </h1>
+      <h1 className="text-red-400">Fetching data from Apollo Graphql server</h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
