34cde5a72a7bf73c010f8c4a2f826c3f659aa022
fuerte <leandrofuerte75@gmail.com>
2025-04-03 20:53:36 +0100

    installing @apollo/client and fixing svg problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import {} from "@apollo/client"
+
 function App() {
   return (
     <main>
-      <h1 className="text-red-400">Fetching data from Apollo Graphql server</h1>
+      <h1 className="text-center text-3xl my-12">Fetching data from Apollo Graphql server</h1>
     </main>
   );
 }
