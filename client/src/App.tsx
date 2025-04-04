1102d3f16987eb7207a22ecbb4422b5addc88539
fuerte <leandrofuerte75@gmail.com>
2025-04-04 10:09:05 +0100

    Creating states to create user and styling form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,9 @@ type User = {
 
 function App() {
   const [id, setId] = useState(1);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [isMarried, setIsMarried] = useState(false);
   const {
     data: getUsersData,
     error: getUsersError,
@@ -22,6 +25,10 @@ function App() {
     error: getUserByIdError,
     loading: getUserByIdLoading,
   } = useGetUserById(id);
+
+  function handleCreateUser() {}
+
+
 
   return (
     <main>
@@ -67,23 +74,26 @@ function App() {
         </ul>
       )}
       <button onClick={() => setId(2)}>change user</button>
-      <form>
+      <form className="w-1/2 mx-auto flex flex-col gap-2 bg-slate-950 p-4 rounded-lg items-center">
         <input
+        className="bg-slate-800 rounded-lg p-2 w-full"
           type="text"
           placeholder="name"
           onChange={(e) => console.log(e.target.value)}
         />
         <input
+        className="bg-slate-800 rounded-lg p-2 w-full"
           type="number"
           placeholder="age"
           onChange={(e) => console.log(e.target.value)}
         />
         <input
+        className="bg-slate-800 rounded-lg p-2 w-full"
           type="checkbox"
           placeholder="isMarried"
           onChange={(e) => console.log(e.target.checked)}
         />
-        <button>Create user</button>
+        <button className="bg-slate-800 hover:bg-slate-700 rounded-lg p-2 w-full" onClick={handleCreateUser}>Create user</button>
       </form>
     </main>
   );
