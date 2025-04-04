4be90eea903ae43d6cf917e9e7ff3f8f12808ebc
fuerte <leandrofuerte75@gmail.com>
2025-04-04 10:11:38 +0100

    Componentizing createUserForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { SlRefresh } from "react-icons/sl";
-import { useGetUsers, useCreateUser, useGetUserById } from "./lib/queries";
+import { useGetUsers, useGetUserById } from "./lib/queries";
 
 type User = {
   name?: string;
@@ -11,9 +11,7 @@ type User = {
 
 function App() {
   const [id, setId] = useState(1);
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [isMarried, setIsMarried] = useState(false);
+
   const {
     data: getUsersData,
     error: getUsersError,
@@ -25,10 +23,6 @@ function App() {
     error: getUserByIdError,
     loading: getUserByIdLoading,
   } = useGetUserById(id);
-
-  function handleCreateUser() {}
-
-
 
   return (
     <main>
@@ -74,27 +68,6 @@ function App() {
         </ul>
       )}
       <button onClick={() => setId(2)}>change user</button>
-      <form className="w-1/2 mx-auto flex flex-col gap-2 bg-slate-950 p-4 rounded-lg items-center">
-        <input
-        className="bg-slate-800 rounded-lg p-2 w-full"
-          type="text"
-          placeholder="name"
-          onChange={(e) => console.log(e.target.value)}
-        />
-        <input
-        className="bg-slate-800 rounded-lg p-2 w-full"
-          type="number"
-          placeholder="age"
-          onChange={(e) => console.log(e.target.value)}
-        />
-        <input
-        className="bg-slate-800 rounded-lg p-2 w-full"
-          type="checkbox"
-          placeholder="isMarried"
-          onChange={(e) => console.log(e.target.checked)}
-        />
-        <button className="bg-slate-800 hover:bg-slate-700 rounded-lg p-2 w-full" onClick={handleCreateUser}>Create user</button>
-      </form>
     </main>
   );
 }
