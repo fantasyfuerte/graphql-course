9ec6ef023fdd85cf6dd1cd58d4af681fa795bb7a
fuerte <leandrofuerte75@gmail.com>
2025-04-04 07:11:31 +0100

    displaying user data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,47 @@
+import { useQuery, useMutation, gql } from "@apollo/client";
+import { SlRefresh } from "react-icons/sl";
+
+const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      id
+      name
+      age
+      isMarried
+    }
+  }
+`;
+
 function App() {
+  const { data, error, loading } = useQuery(GET_USERS);
+
   return (
     <main>
       <h1 className="text-center text-3xl my-12">
         Fetching data from Apollo Graphql server
       </h1>
+      <div>
+        <h4 className="text-xl text-center">Users:</h4>
+      </div>
+      {loading ? (
+        <div className="animate-spin">
+          <SlRefresh size={36} />
+        </div>
+      ) : error ? (
+        <p className="text-xl text-center opacity-70 font-semibold mt-6">
+          Error: {error.message}
+        </p>
+      ) : (
+        <ul>
+          {data.getUsers.map((user) => {
+            <li key={user.id}>
+              <h4>{user.name}</h4>
+              <p>{user.age}</p>
+              <p>It's {!user.isMarried && "not"} married</p>
+            </li>;
+          })}
+        </ul>
+      )}
     </main>
   );
 }
